dda3538b666e71c68abcd4c672b9d902e5dcd919
JSD <js.dohil@gmail.com>
2024-11-11 22:16:22 -0800

    package file fixes

--- server/src/server.ts
+++ server/src/server.ts
@@ -56,7 +56,7 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
       res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
